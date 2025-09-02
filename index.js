09bb438cde36b5dd628659bc71cb4b20c80edc08
castrokimaru <castrokimaru.com>
2025-09-02 15:51:30 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code in this file!
-const currentUsr = 'Castro kimaru'
-const welcomeMessage = `Welcome to Flatbook, ${currentUsr}!`
+const currentUser = 'Castro kimaru'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
-const shortGreeting = `Welcome, ${currentUsr.slice(0,1)}!`
-const firstInitial = currentUsr.slice(0,1)
-const lastName = currentUsr.slice(7)
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+const firstInitial = currentUser.slice(0,1)
+const lastName = currentUesr.slice(7)
 const shortGreeting2 = `Welcome, ${firstInitial}!`          
 const shortGreeting3 = `Welcome, ${firstInitial} ${lastName}!`
